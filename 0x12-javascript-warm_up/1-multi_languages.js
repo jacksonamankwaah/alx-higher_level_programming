505053bd97a1a25c3dbd88a3d4e87e70d6020f39
jacksonamankwaah <amankwaahjackson@gmail.com>
2024-06-19 08:52:54 -0700

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* script that print 3 lines */
+'C is fun';
+'Python is cool';
+'JavaScript is amazing';
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
